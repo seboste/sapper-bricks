afb3d70b928cb6e50bfaf9f843d06194d9b0a769
Steger, Sebastian <Sebastian.Steger@gmail.com>
2023-03-02 21:30:35 +0100

    add ExceptionHandlingRequestHook

--- base/hexagonal-skeleton/src/app/main.cpp
+++ base/hexagonal-skeleton/src/app/main.cpp
@@ -3,7 +3,9 @@
 #include <microservice-essentials/context.h>
 #include <microservice-essentials/handler.h>
 #include <microservice-essentials/utilities/environment.h>
+#include <microservice-essentials/cross-cutting-concerns/exception-handling-request-hook.h>
 #include <microservice-essentials/cross-cutting-concerns/graceful-shutdown.h>
+#include <microservice-essentials/request/request-processor.h>
 //<<<SAPPER SECTION BEGIN MAIN-INCLUDES>>>
 //<<<SAPPER SECTION END MAIN-INCLUDES>>>
 
@@ -15,6 +17,10 @@ int main()
             {"app", mse::getenv_or("APP", "test-grpc") },
             {"version", mse::getenv_or("VERSION", "1.0.0") }
         });
+
+//<<<SAPPER SECTION BEGIN MAIN-REQUEST-HOOKS>>>
+    mse::RequestHandler::GloballyWith(mse::ExceptionHandlingRequestHook::Parameters{});
+//<<<SAPPER SECTION END MAIN-REQUEST-HOOKS>>>
     
 //<<<SAPPER SECTION BEGIN MAIN-REPO-INSTANTIATION>>>
     std::unique_ptr<EntityRepo> repo = nullptr;
